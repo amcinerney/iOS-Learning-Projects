dd070d4bc18c26c0f18a9bfb58965f9a20333479
Aaron McInerney <amcinerney@admins-macbook-air.local>
2017-02-01 21:19:09 -0500

    Chapter one quiz finished

--- Quiz/Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/Quiz/ViewController.swift
@@ -1,25 +1,43 @@
-//
-//  ViewController.swift
-//  Quiz
-//
-//  Created by Guest User on 1/31/17.
-//  Copyright © 2017 Big Nerd Ranch. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "what is 7 + 7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    
+    var currentQuestionIndex: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func showNextQuestion(_ sender: UIButton){
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
     }
-
+    
+    @IBAction func showAnswer(_ sender: UIButton){
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
 
 }
 
